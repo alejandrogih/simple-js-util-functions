ae17a011f3476ff9047e6ec40fbba05f30d961d8
jyepes <jyepes@hugeinc.com>
2019-10-09 14:50:53 -0500

    add more date-time utils

--- src/date-time.js
+++ src/date-time.js
@@ -15,3 +15,30 @@ export const getWeekDays = (padd) => {
 
     return `${firstday} - ${lastday}`;
 };
+
+/*
+ @param dmy boolean - true is want output date in dd/mm/yyyy format
+ */
+export const currentDate = (dmy = false) => {
+    const current_datetime = new Date();
+    if (dmy) {
+      return `${current_datetime.getDate().toString().padStart(2, 0)}/${(current_datetime.getMonth() + 1).toString().padStart(2, 0)}/${current_datetime.getFullYear()}`;
+    }
+  
+    return `${(current_datetime.getMonth() + 1).toString().padStart(2, 0)}/${current_datetime.getDate().toString().padStart(2, 0)}/${current_datetime.getFullYear()}`;
+}
+    
+/* 
+ @param date string - The date to check.
+ */
+export const isTodayOrFuture = (date) => new Date(date) >= new Date(currentDay());
+
+/* 
+ @param arr Array - An array of dates to sort
+ */
+export const sortByDate = (arr) => arr.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+/* 
+ @param arr Array - An array of dates to sort
+ */
+export const sortByTime = (arr) => arr.sort((a, b) => new Date(`${a.date} ${a.hour}`) - new Date(`${b.date} ${b.hour}`));
